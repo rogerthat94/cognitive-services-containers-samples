660a7984995c7789107219c1204f6b9ab4babd61
Roger Iyengar <ri@rogeriyengar.com>
2019-11-26 17:03:00 -0500

    Update FaceHttp.py

--- python/Face/FaceHttp/FaceHttp.py
+++ python/Face/FaceHttp/FaceHttp.py
@@ -1,4 +1,6 @@
+import cv2
 import requests
+import numpy as np
 from pprint import pprint
 
 face_uri = "http://localhost:5000/face/v1.0/detect?returnFaceAttributes=*"
@@ -7,9 +9,24 @@ pathToFileInDisk = r'<path to image>'
 
 with open( pathToFileInDisk, 'rb' ) as f:
     data = f.read()
-headers = { "Content-Type": "image/jpeg" }
+    headers = { "Content-Type": "image/jpeg" }
 
-response = requests.post(face_uri, headers=headers, data=data)
-faces = response.json()
+    response = requests.post(face_uri, headers=headers, data=data)
+    faces = response.json()
 
-pprint(faces)
+    pprint(faces)
+
+    np_data = np.fromstring(data, dtype=np.uint8)
+    img = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+    for face in faces:
+        rectangle = face[u'faceRectangle']
+        height = rectangle[u'height']
+        left = rectangle[u'left']
+        top = rectangle[u'top']
+        width = rectangle[u'width']
+
+        cv2.rectangle(img, (left, top), ((left + width), (top + height)), (123, 174, 54), 3)
+
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
